565fe335c1cef96687ed34f7262e6ff4ad3fb3a2
550568261@qq.com <550568261@qq.com>
2016-10-29 10:51:34 +0800

    DLL

--- trunk/src/DeviceManagementSystem/NYB.DeviceManagementSystem.BLL/Class1.cs
+++ trunk/src/DeviceManagementSystem/NYB.DeviceManagementSystem.BLL/Class1.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NYB.DeviceManagementSystem.DAL;
 
 namespace NYB.DeviceManagementSystem.BLL
 {
     public class Class1
     {
+        public void Test()
+        {
+            using (var context = new DeviceMgmtEntities())
+            {
+
+            }
+        }
     }
 }
