153b49c2f04b5f6b0ce4d1d44f204ee9a5595ab6
gaojingzong <gjzgaojingzong@163.com>
2016-11-01 22:06:22 +0800

    sssss

---
+++ trunk/src/DeviceManagementSystem/NYB.DeviceManagementSystem.ViewModel/User.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYB.DeviceManagementSystem.ViewModel
+{
+    public class User
+    {
+        private string _userName = string.Empty;
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+
+        private string _pwd = string.Empty;
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Pwd
+        {
+            get { return _pwd; }
+            set { _pwd = value; }
+        }
+
+        private string _logoName = string.Empty;
+
+        /// <summary>
+        /// 登录名
+        /// </summary>
+        public string LogoName
+        {
+            get { return _logoName; }
+            set { _logoName = value; }
+        }
+
+        private string _address = string.Empty;
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        private string _telPhone = string.Empty;
+
+        /// <summary>
+        /// 联系电话
+        /// </summary>
+        public string TelPhone
+        {
+            get { return _telPhone; }
+            set { _telPhone = value; }
+        }
+
+        private string _projectID = string.Empty;
+
+        /// <summary>
+        /// 项目所属
+        /// </summary>
+        public string ProjectID
+        {
+            get { return _projectID; }
+            set { _projectID = value; }
+        }
+        private string _email = string.Empty;
+        /// <summary>
+        /// 电子邮件
+        /// </summary>
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+
+    }
+}
