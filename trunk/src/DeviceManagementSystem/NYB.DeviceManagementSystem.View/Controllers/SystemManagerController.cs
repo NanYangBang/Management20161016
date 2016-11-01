86d360f15aab314b69f4191fbd4c0f544ff4d185
gaojingzong <gjzgaojingzong@163.com>
2016-11-01 22:06:09 +0800

    sss

---
+++ trunk/src/DeviceManagementSystem/NYB.DeviceManagementSystem.View/Controllers/SystemManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NYB.DeviceManagementSystem.View.Controllers
+{
+    public class SystemManagerController : Controller
+    {
+        //
+        // GET: /SystemManager/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
