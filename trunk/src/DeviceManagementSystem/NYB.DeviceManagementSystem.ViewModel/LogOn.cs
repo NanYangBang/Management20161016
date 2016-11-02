b339fc8da59d491110568061ebdd4ca2b0e96524
gaojingzong <gjzgaojingzong@163.com>
2016-11-02 21:08:25 +0800

    werw

---
+++ trunk/src/DeviceManagementSystem/NYB.DeviceManagementSystem.ViewModel/LogOn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYB.DeviceManagementSystem.ViewModel
+{
+    public class LogOn
+    {
+        string userName = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+    }
+}
