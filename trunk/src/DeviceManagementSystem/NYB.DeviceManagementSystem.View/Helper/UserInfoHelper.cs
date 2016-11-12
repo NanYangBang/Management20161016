33d9b696f7c271d047d1cb04834ac6601ca7f1d4
550568261@qq.com <550568261@qq.com>
2016-11-12 16:51:08 +0800

    好

---
+++ trunk/src/DeviceManagementSystem/NYB.DeviceManagementSystem.View/Helper/UserInfoHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace System.Web.Mvc
+{
+    public static class UserInfoHelper
+    {
+        public static string GetCurrentProjectID(this Controller controller)
+        {
+            return controller.Request.Cookies["CurrentProjectID"].Value;
+        }
+
+        public static string GetCurrentUserID(this Controller controller)
+        {
+            return controller.Request.Cookies["CurrentUserID"].Value;
+        }
+    }
+}
