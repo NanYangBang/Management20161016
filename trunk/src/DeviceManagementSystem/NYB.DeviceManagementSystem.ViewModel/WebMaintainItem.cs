ed37fff0d4b4282a72ff5984dbb02e3f3c3484a4
zlm <21>
2017-03-24 17:42:32 +0800

    保养类别

--- trunk/src/DeviceManagementSystem/NYB.DeviceManagementSystem.ViewModel/WebMaintainItem.cs
+++ trunk/src/DeviceManagementSystem/NYB.DeviceManagementSystem.ViewModel/WebMaintainItem.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +11,22 @@ namespace NYB.DeviceManagementSystem.ViewModel
     public class WebMaintainItem : ViewModelBase
     {
         public string ID { get; set; }
+
+        [DisplayName("名称")]
+        [Required]
+        [StringLength(30, ErrorMessage = "最大长度为30")]
         public string Name { get; set; }
-        public string Note { get; set; }
+
+        private string _note = string.Empty;
+        [DisplayName("备注")]
+        [StringLength(100, ErrorMessage = "最大长度为100")]
+        public string Note
+        {
+            get { return _note; }
+            set
+            {
+                _note = value;
+            }
+        }
     }
 }
